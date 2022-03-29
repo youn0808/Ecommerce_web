9de5d7223c032d7f12486920dee4c2e141a7170f
Seung.Y <hhwannn2@gmail.com>
2022-03-29 03:05:01 -0500

    add react router (link)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-          <HomeScreen/>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
-  )
-}
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-        <Container> 
-          <Navbar.Brand href='/'>bestbuy</Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto' >
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart'></i> Cart
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            bestbuy
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/cart">
+                <i className="fas fa-shopping-cart"></i> Cart
               </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user'></i> Sign In
+              <Nav.Link as={Link} to="/login">
+                <i className="fas fa-user"></i> Sign In
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
