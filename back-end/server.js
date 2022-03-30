dc8c9bff3cefbd3f7922dcf2a3c78784bbc581b0
Seung.Y <hhwannn2@gmail.com>
2022-03-30 03:21:29 -0500

    change ES module

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,11 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+// const express = require("express");
+// const dotenv = require("dotenv");
+// const products = require("./data/products");
+
+dotenv.config();
 
 const app = express();
 
@@ -15,5 +21,9 @@ app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log("Server running on port 5000"));
+app.listen(
+  5000,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
