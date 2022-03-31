32cb67427df0efb575c9fea0ab3b991fbdd1dec3
Seung.Y <hhwannn2@gmail.com>
2022-03-31 03:56:07 -0500

    add error handling

---
+++ back-end/middleware/errorMiddleware.js
@@ -0,0 +1,19 @@
+const notFound = (req, res, next) => {
+  // when user try to access wrong url
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  //set if there is an error make it return 500 so that we can see the what is the error
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+// import products from "./data/products.js";
+
 // const express = require("express");
 // const dotenv = require("dotenv");
 // const products = require("./data/products");
 
 dotenv.config();
+
+//connect data base.
+
+connectDB();
 
 const app = express();
 
@@ -13,17 +23,24 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
+
+// app.get("/api/products", (req, res) => {
+//   res.json(products);
+// });
+
+// app.get("/api/products/:id", (req, res) => {
+//   const product = products.find((p) => p._id === req.params.id);
+//   res.json(product);
+// });
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   5000,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
