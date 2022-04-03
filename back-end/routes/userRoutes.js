4a72e545441d8eeea2d27f87e8aa85eca6e4afea
Seung.Y <hhwannn2@gmail.com>
2022-04-03 16:08:59 -0500

    custom auth middleware

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,7 +1,8 @@
-import express from 'express'
-const router = express.Router()
-import { authUser } from '../controllers/userController.js'
+import express from "express";
+const router = express.Router();
+import { authUser, getUserProfile } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
+router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
 
-router.post('/login', authUser)
-
-export default router
+export default router;
