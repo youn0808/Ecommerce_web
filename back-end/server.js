588d10c6e19c318bcf651087627d960f5e41a500
Seung.Y <hhwannn2@gmail.com>
2022-04-03 21:48:48 -0500

    order controller & route

--- back-end/server.js
+++ back-end/server.js
@@ -6,7 +6,7 @@ import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-
+import orderRoutes from "./routes/orderRoutes.js";
 // import products from "./data/products.js";
 
 // const express = require("express");
@@ -29,6 +29,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
