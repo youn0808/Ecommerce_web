52bd9b274e8def0724d22db529cf69b3d974c5fa
Seung.Y <hhwannn2@gmail.com>
2022-04-03 21:32:49 -0500

    checkout step componenet

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,6 +9,8 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+
 const App = () => {
   return (
     <Router>
@@ -16,6 +18,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Routes>
+            <Route path="/shipping" element={<ShippingScreen />} />
             <Route path="/login" element={<LoginScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
             <Route path="/profile" element={<ProfileScreen />} />
