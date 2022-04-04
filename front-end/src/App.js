fdcc3855328cea95492d78a01d65dfd48dbc2300
Seung.Y <hhwannn2@gmail.com>
2022-04-03 20:50:04 -0500

    profile screen & get user details

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 const App = () => {
   return (
     <Router>
@@ -16,8 +17,9 @@ const App = () => {
         <Container>
           <Routes>
             <Route path="/login" element={<LoginScreen />} />
-            <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart" element={<CartScreen />}>
               <Route path=":id" element={<CartScreen />} />
             </Route>
