3e750a550def2c673631f2821e66541e938b4978
Seung.Y <hhwannn2@gmail.com>
2022-04-07 02:41:21 -0500

    admin create product fix

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -17,6 +17,8 @@ import UserListScreen from "./screens/UserListScreen";
 
 import UserEditScreen from "./screens/UserEditScreen";
 import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
+
 const App = () => {
   return (
     <Router>
@@ -37,6 +39,10 @@ const App = () => {
             </Route>
             <Route path="/admin/userlist" element={<UserListScreen />} />
             <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
+            <Route
+              path="/admin/product/:id/edit"
+              element={<ProductEditScreen />}
+            />
             <Route path="/admin/productlist" element={<ProductListScreen />} />
             <Route path="/" element={<HomeScreen />} exact />
           </Routes>
